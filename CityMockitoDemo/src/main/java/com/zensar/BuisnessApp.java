ebb9a74b2ae09ddd494d930976bbf2f430ea6ac6
AG66200 <AG66200@RMGW63ZLPC0475.ind.zensar.com>
2022-03-30 16:37:44 +0530

    second commit

--- CityMockitoDemo/src/main/java/com/zensar/BuisnessApp.java
+++ CityMockitoDemo/src/main/java/com/zensar/BuisnessApp.java
@@ -16,6 +16,7 @@ public class BuisnessApp {
 		List<String> cities =  this.cityService.findCitiesByCountries(country);
 		return cities;
 	}
+	//this is a change
 	
 	public boolean login(String username,String password) {
 		return loginService.authenticate(username, password);
